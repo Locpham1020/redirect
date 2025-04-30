6966774fe4bc0f5d7984f6dc1b034bc81f027fa6
Locpham1020 <locpv.1020@gmail.com>
2025-04-30 15:01:24 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,12 +1,19 @@
 var PRODUCT_DATA = {
   "sp01": {
     "id_container": "sp01",
-    "money": "200",
-    "link_shopee": "https://www.tiktok.com",
-    "link_tiktok": "https://shopee.vn"
+    "money": "200,000",
+    "link_shopee": "https://shopee.vn/product1",
+    "link_tiktok": "https://www.tiktok.com/@user/video/id1"
   },
+  "sp02": {
+    "id_container": "sp02",
+    "money": "150,000",
+    "link_shopee": "https://shopee.vn/product2",
+    "link_tiktok": "https://www.tiktok.com/@user/video/id2"
+  },
+  // Thêm các sản phẩm khác tại đây
   "_metadata": {
-    "last_updated": "2025-04-30T10:00:00Z",
-    "version": "4.2.0"
+    "last_updated": "2025-05-01T10:00:00Z",
+    "version": "4.2.1"
   }
 };

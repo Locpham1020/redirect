3157853d13badbe440798f8f9bf5ea7ac363082e
Locpham1020 <locpv.1020@gmail.com>
2025-04-30 17:34:54 +0700

    Update data.js

--- data.js
+++ data.js
@@ -3,28 +3,12 @@ handleProductData({
     "id_container": "sp01",
     "money": 999,
     "link_shopee": "https://translate.google.com/?hl=vi&sl=en&tl=vi&op=translate",
-    "link_tiktok": "https://google.com/",
-    "last_updated": "2023-04-30T10:20:55.042Z"
+    "link_tiktok": "https://google.com/"
   },
   "sp02": {
     "id_container": "sp02",
     "money": 1234567,
     "link_shopee": "https://google.com/",
-    "link_tiktok": "https://shopee.vn/MSC3MALY-XEJNOAAZNH-MSC3MALY-Quay-Phim-cat.11036101",
-    "last_updated": "2023-04-30T10:20:55.042Z"
-  },
-  "sp03": {
-    "id_container": "sp03",
-    "money": 1234567,
-    "link_shopee": "https://google.com/",
-    "link_tiktok": "https://shopee.vn/MSC3MALY-XEJNOAAZNH-MSC3MALY-Quay-Phim-cat.11036101",
-    "last_updated": "2023-04-30T10:20:55.042Z"
-  },
-  "sp04": {
-    "id_container": "sp04",
-    "money": 777888999,
-    "link_shopee": "https://google.com/",
-    "link_tiktok": "https://shopee.vn/MSC3MALY-XEJNOAAZNH-MSC3MALY-Quay-Phim-cat.11036101",
-    "last_updated": "2023-04-30T10:20:55.042Z"
+    "link_tiktok": "https://shopee.vn/MSC3MALY-XEJNOAAZNH-MSC3MALY-Quay-Phim-cat.11036101"
   }
 });
